cb6417decc90046550ff8117abcee1be2b9c9b19
daniellromo <daniel.roshanmoniri@gmail.com>
2024-03-21 01:50:26 +0000

    changed logic to accept user name and age to output message in console

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,3 @@
-user_input = input("Enter text: ")
-print(text)
+user_input_name = input("Enter name: ")
+user_input_age = input("Enter age: ")
+print("you are " + user_input_name + " and you are " + user_input_age + " years old!")
